f59c83d20017e67bdd2f9f12fde9f74d5f5dc770
mgl-uhou <miguelferreiralaurentino@gmail.com>
2023-11-16 14:33:14 -0300

    finalizando o projetinho

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
+const input = document.getElementsByTagName('input')[0];
+
 const result = () => {
     console.log(document.getElementsByTagName('input')[0].value);
-    console.log(eval(document.getElementsByTagName('input')[0].value));
+    console.log(eval(document.getElementsByTagName('input')[0].value.replace(/\^/g, "**")));
 
-    const input = document.getElementsByTagName('input')[0];
-
-    const num = eval(document.getElementsByTagName('input')[0].value);
+    const num = eval(document.getElementsByTagName('input')[0].value.replace(/\^/g, "**"));
 
     input.value = num.toString();
 }
 
 const addCaracters = (x) => {
-    const input = document.getElementsByTagName('input')[0];
-
     let num = x;
     input.value += num;
 }
 
 const clearInput = () => {
-    const input = document.getElementsByTagName('input')[0];
     input.value = "";
 }
+
+const replace = () => {
+    input.value = eval(document.getElementsByTagName('input')[0].value * (-1));
+}
+
+/* const percent = () => {
+    
+} */
